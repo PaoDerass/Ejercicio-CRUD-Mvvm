b97d624e280fd4c8888961181f5c4ebec82ca2d0
PaoDerass <paola_deras@unitec.edu>
2024-09-18 22:41:35 -0600

    calculos base de datos

---
+++ BD/ProveedorDatabase.cs
@@ -0,0 +1,40 @@
+﻿using SQLite;
+using EjercicioCRUDMvvm.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace EjercicioCRUDMvvm.Services
+{
+    public class ProveedorDatabase
+    {
+        private readonly SQLiteAsyncConnection _database;
+
+        public ProveedorDatabase(string dbPath)
+        {
+            _database = new SQLiteAsyncConnection(dbPath);
+            _database.CreateTableAsync<Proveedor>().Wait();
+        }
+
+        public Task<List<Proveedor>> GetProveedoresAsync()
+        {
+            return _database.Table<Proveedor>().ToListAsync();
+        }
+
+        public Task<int> SaveProveedorAsync(Proveedor proveedor)
+        {
+            if (proveedor.Id != 0)
+            {
+                return _database.UpdateAsync(proveedor);
+            }
+            else
+            {
+                return _database.InsertAsync(proveedor);
+            }
+        }
+
+        public Task<int> DeleteProveedorAsync(Proveedor proveedor)
+        {
+            return _database.DeleteAsync(proveedor);
+        }
+    }
+}
