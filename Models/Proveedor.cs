a63ef8855266924a15bda41fe2f9ae276c34b201
PaoDerass <paola_deras@unitec.edu>
2024-09-18 22:36:54 -0600

    tabla proveedor

---
+++ Models/Proveedor.cs
@@ -0,0 +1,14 @@
+﻿using SQLite;
+
+namespace EjercicioCRUDMvvm.Models
+{
+    public class Proveedor
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Telefono { get; set; }
+        public string Empresa { get; set; }
+        public string Direccion { get; set; }
+    }
+}
